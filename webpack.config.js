105c6f2ac13224c2898ab0a18fb5a1c9e5b01ed0
Kamal Reddy <kamal.is.reddy@gmail.com>
2017-11-07 08:13:00 +0530

    initial

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var config = {
+    entry: './src/index.js',
+
+    output: {
+        path:'/',
+        filename: 'm.js',
+    },
+
+    devServer: {
+        inline: true,
+        port: 8000,
+        proxy: {
+            '/api': {
+                target: {
+                    host: "0.0.0.0",
+                    protocol: 'http:',
+                    port: 8080
+                },
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
