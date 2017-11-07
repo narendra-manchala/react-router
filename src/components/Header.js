68e0dca91f92a3ba361b21890295947f0fab2fcb
Kamal Reddy <kamal.is.reddy@gmail.com>
2017-11-07 08:28:05 +0530

    initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
   <header>
     <nav>
       <ul>
-        <li><Link to='/'>Home</Link></li>
+        <li><Link to=''>Home</Link></li>
         <li><Link to='/roster'>Roster</Link></li>
         <li><Link to='/schedule'>Schedule</Link></li>
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './components/App';
 
 render((
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 ), document.getElementById('root'));
